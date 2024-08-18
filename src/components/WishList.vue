478a18c10cfa4a858d0efbd719de588ce6bfcbd1
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-08-18 12:40:44 +0200

    "Added grid layout and product item template to WishList component"

--- src/components/WishList.vue
+++ src/components/WishList.vue
@@ -4,5 +4,22 @@
         <div v-if="wishlist.length ===0" class="text-center text-gray-500">
             <p>Your wishlist is empty. Start adding products you like!</p>
         </div>
+
+        <div v-else class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div
+            v-for="item in wishlist"
+            :key="item.id"
+            class="bg-white p-4 rounded-lg shadow-md"
+            >
+            <img
+            :src="item.image"
+            alt="Product Image"
+            class="w-full h-40 object-contain rounded-t-lg"
+            />
+
+            </div>
+
+            
+        </div>
     </div>
 </template>
