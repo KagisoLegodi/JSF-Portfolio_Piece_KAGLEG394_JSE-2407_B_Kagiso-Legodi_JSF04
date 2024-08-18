45d8672e1bf7e9efb50d51e1fbfd51ec7c556d46
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-08-18 13:40:00 +0200

    "Added new imports and routes to router.js"

--- src/router.js
+++ src/router.js
@@ -1,6 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
-import ProductList from "./components/ProductList.vue";
-import ProductDetail from "./components/ProductDetail.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import ProductList from './components/ProductList.vue';
+import ProductDetail from './components/ProductDetail.vue';
+import Login from './components/Login.vue';
+import Cart from './components/Cart.vue';
+import WishList from './components/WishList.vue';
+import ComparisonPage from './views/comparisonPage.vue'; // Import the ComparisonPage component
+import { useLoginStore } from './stores/loginStore';
 
 const routes = [
   { path: "/", component: ProductList },
