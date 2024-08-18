340fe9052255caddfc87917a01aee4d699aa6881
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-08-18 13:28:27 +0200

    "Added Pinia and FontAwesome imports, registered FontAwesomeIcon component globally, and updated app creation to use Pinia and router"

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router"; // We will set up this file next
+import router from "./router";
+import { createPinia } from "pinia";
 
-createApp(App).use(router).mount("#app");
+// FontAwesome Imports
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faShoppingCart, faStar, faInfoCircle,faTrash,faFilter,faBalanceScale,faHome} from "@fortawesome/free-solid-svg-icons";
+
+// Add the shopping cart icon to the library
+library.add(faShoppingCart, faStar, faInfoCircle,faTrash,faFilter,faBalanceScale,faHome);
+
+const pinia = createPinia();
+
+const app = createApp(App);
+
+// Register FontAwesome component globally
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+// Use Pinia and Vue Router
+app.use(router).use(pinia).mount("#app");
