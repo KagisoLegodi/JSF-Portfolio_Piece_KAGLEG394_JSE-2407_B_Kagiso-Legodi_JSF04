38c0f13604e383d7f0acd754047bfe8bdbec7795
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-08-18 13:27:39 +0200

    "Replace Vuex with Pinia, add state and actions to mainStore"

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,3 +1,20 @@
+import { defineStore } from "pinia";
+
+export const useStore = defineStore("mainStore", {
+  state: () => ({
+    selectedCategory: "",
+    sortOrder: "",
+  }),
+  actions: {
+    updateCategory(category) {
+      this.selectedCategory = category;
+    },
+    updateSortOrder(sortOrder) {
+      this.sortOrder = sortOrder;
+    },
+  },
+});
+
 // import { createStore } from "vuex";
 
 // export default createStore({
