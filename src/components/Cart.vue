fb23e4d088114a3de7dd9c931811e55b14140b3c
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-08-21 13:05:42 +0200

    refactor cart : Updated the table width to be fully responsive with `w-full`. Changed the routing behavior for the "Go Back to Products" button to navigate to the '/products' route.

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -10,7 +10,7 @@
     <!-- Display the cart items if the cart is not empty -->
     <div v-else class="overflow-x-auto">
       <table
-        class="min-w-full text-sm text-left rtl:text-left text-gray-500 dark:text-gray-400"
+        class="w-full table-auto text-sm text-left rtl:text-left text-gray-500 dark:text-gray-400"
       >
         <thead
           class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
@@ -136,6 +136,6 @@ const addToComparison = (item) => {
 };
 
 const goBackToProducts = () => {
-  router.push("/");
+  router.push("/products");
 };
 </script>
